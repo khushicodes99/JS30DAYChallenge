6e5b1ba635ea137c2151a85bbc1b6cbb5d7a507f
Khushi Bajaj <khushboobajaj0153@gmail.com>
2023-01-01 19:26:16 -0700

    commit

---
+++ JS_1.js
@@ -0,0 +1,22 @@
+//1st day of my 30 day JSCODE Challenege
+
+//Program to add 2 data types
+let str = "First Day"
+let add = 1
+console.log(str + add)
+
+//Program to find data type of above addition
+let res = str + add
+console.log(typeof res)
+
+//Creating one object
+let obj = {
+  name: "John" ,
+  age: "25" ,
+  height: "5.5"
+}
+console.log(obj)
+//Adding another value to the object
+obj.salary = "25k"
+console.log(obj)
+
